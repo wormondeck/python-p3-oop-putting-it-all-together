c86007a72e1322c500e3ffce89678a55af5bbf92
Ronney Arias <ronneyarias@gmail.com>
2024-05-14 10:55:02 -0500

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.current_page = 1
+
+    def turn_page(self):
+        self.current_page += 1
+        print("Flipping the page...wow, you read fast!")
     
-        
+    @property
+    def page_count(self):
+        return self._page_count
+            
+    @page_count.setter
+    def page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print('page_count must be an integer')
+        else:
+            self._page_count = page_count
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition = "New"):
+        self.size = size
+        self.brand = brand
+        self.repaired_shoe = True
+        self.condition = condition
+
+    def repair(self):
+        self.condition
+    
+    def cobble(self):
+        self.repaired_shoe = True
+        print('Your shoe is as good as new!')
+        
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print('size must be an integer')
+        else:
+            self._size = value
